523c141e748994787c36467a35e4a53377f531b5
joaohenrique <joao.sarraceni@gmail.com>
2021-10-27 18:20:37 +0200

    Assync implementation with unique ID users

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
 import json
 import datetime
 from tinydb import TinyDB, Query
-import grequests
-
+import aiohttp
+import asyncio
 app = FastAPI()
 
 
@@ -24,40 +24,50 @@ async def show_data(data_in: InputsPost):
     # build or create json database
     database = TinyDB('weather_data.json')
 
+    # authorization user unique id
+    input_user = Query()
+    if database.search(input_user.userID == data_in.id):
+        raise HTTPException(status_code=401, detail=f"There's already the User ID: {data_in.id} in database")
+
     # load list of cities IDs
     with open('cities.txt', 'rb') as open_file:
         city_ids = json.load(open_file)
-    # all cities weather data
 
+    # all cities weather data
     progress = 0    # show the progress of requests
-    for city_id in city_ids:
-        # weather request
-        response = requests.get(
-            "http://api.openweathermap.org/data/2.5/weather?id=" + str(city_id) + "&appid=b2626a09f300fa0361d32b9ef5b208ff&units=metric")
-        # create the dict with weather data
-        if progress == 0:
-            weather_report = {
-                'localtime': json.dumps(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))),
-                'userID': data_in.id,
-                'locals': [
-                    {
+
+    async with aiohttp.ClientSession() as session:
+
+        for city_id in city_ids:
+            # weather url
+            weather_url = "http://api.openweathermap.org/data/2.5/weather?id="+str(city_id)+"&appid=b2626a09f300fa0361d32b9ef5b208ff&units=metric"
+            # start async request
+            async with session.get(weather_url) as resp:
+                response = await resp.json()
+
+                # create the dict with weather data
+                if progress == 0:
+                    weather_report = {
+                        'localtime': json.dumps(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))),
+                        'userID': data_in.id,
+                        'locals': [
+                            {
+                                'local_id': city_id,
+                                'temp_c': response['main']['temp'],
+                                'humidity': response['main']['humidity']
+                            }]}
+                    progress += 1
+                else:
+                    add_data = {
                         'local_id': city_id,
-                        'temp_c': response.json()['main']['temp'],
-                        'humidity': response.json()['main']['humidity']
-                    }]}
-            progress += 1
-        else:
-            add_data = {
-                'local_id': city_id,
-                'temp_c': response.json()['main']['temp'],
-                'humidity': response.json()['main']['humidity']
-                        }
-            progress += 1
-            # append new local to weather_report
-            weather_report.get('locals').append(add_data)
+                        'temp_c': response['main']['temp'],
+                        'humidity': response['main']['humidity']
+                                }
+                    progress += 1
+                    # append new local to weather_report
+                    weather_report.get('locals').append(add_data)
 
     database.insert(weather_report)
 
-    ident = Query()
-    return database.search(ident.userID == 1)
+    return database.search(input_user.userID > 0)
 
