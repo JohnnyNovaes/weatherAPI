d50518f79a6528ce7ce3eacfb123d13433dbd4cb
joaohenrique <joao.sarraceni@gmail.com>
2021-10-27 22:11:55 +0200

    GET implemented

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import pandas as pd
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import requests
 import json
 import datetime
 from tinydb import TinyDB, Query
 import aiohttp
 import asyncio
+from tqdm.asyncio import trange, tqdm
+
 app = FastAPI()
 
 
@@ -15,12 +15,16 @@ class InputsPost(BaseModel):
 
 
 @app.get("/")
-async def root(data: InputsPost):
-    return {"message": "Hello World"}
+async def root(data_in):
+    # build or create json database
+    database = TinyDB('weather_data.json')
+    input_user = Query()
+
+    return database.search(input_user.userID == int(data_in))
 
 
 @app.post('/show_data')
-async def show_data(data_in: InputsPost):
+def show_data(data_in: InputsPost):
     # build or create json database
     database = TinyDB('weather_data.json')
 
@@ -29,24 +33,36 @@ async def show_data(data_in: InputsPost):
     if database.search(input_user.userID == data_in.id):
         raise HTTPException(status_code=401, detail=f"There's already the User ID: {data_in.id} in database")
 
+    # weather data async
+    asyncio.run(build_database(data_in))
+    database = TinyDB('weather_data.json')
+
+    return database.search(input_user.userID > 0)
+
+
+async def build_database(data_in):
+    # build or create json database
+    database = TinyDB('weather_data.json')
+
     # load list of cities IDs
     with open('cities.txt', 'rb') as open_file:
         city_ids = json.load(open_file)
 
     # all cities weather data
-    progress = 0    # show the progress of requests
-
+    flag = True
+    # start the async task
     async with aiohttp.ClientSession() as session:
 
         for city_id in city_ids:
             # weather url
-            weather_url = "http://api.openweathermap.org/data/2.5/weather?id="+str(city_id)+"&appid=b2626a09f300fa0361d32b9ef5b208ff&units=metric"
+            weather_url = "http://api.openweathermap.org/data/2.5/weather?id=" + str(
+                city_id) + "&appid=b2626a09f300fa0361d32b9ef5b208ff&units=metric"
             # start async request
             async with session.get(weather_url) as resp:
                 response = await resp.json()
 
                 # create the dict with weather data
-                if progress == 0:
+                if flag:
                     weather_report = {
                         'localtime': json.dumps(str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))),
                         'userID': data_in.id,
@@ -56,18 +72,14 @@ async def show_data(data_in: InputsPost):
                                 'temp_c': response['main']['temp'],
                                 'humidity': response['main']['humidity']
                             }]}
-                    progress += 1
+                    flag = False
                 else:
                     add_data = {
                         'local_id': city_id,
                         'temp_c': response['main']['temp'],
                         'humidity': response['main']['humidity']
-                                }
-                    progress += 1
+                    }
                     # append new local to weather_report
                     weather_report.get('locals').append(add_data)
 
     database.insert(weather_report)
-
-    return database.search(input_user.userID > 0)
-
